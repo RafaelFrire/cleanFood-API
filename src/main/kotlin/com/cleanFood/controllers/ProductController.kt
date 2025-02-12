b32d98337228cf54cb774cc6d3353eede243ee73
RafaelFrire <rafaelfesousa@hotmail.com>
2025-02-12 00:09:30 -0300

    feat: creted methods findById and findMany products

--- src/main/kotlin/com/cleanFood/controllers/ProductController.kt
+++ src/main/kotlin/com/cleanFood/controllers/ProductController.kt
@@ -4,9 +4,13 @@ import com.cleanFood.dtos.ProductResponseDTO
 import com.cleanFood.entitys.Product
 import com.cleanFood.services.ProductService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 
 @RestController
@@ -15,11 +19,20 @@ class ProductController {
     @Autowired
     val productService: ProductService = ProductService();
 
+    @GetMapping("/products")
+    fun getAllProduct():List<Product>{
+        return this.productService.findMany();
+    }
+
+    @GetMapping("/product/{id}")
+    fun findById(@PathVariable(value = "id") id: String):Optional<Product>{
+        return this.productService.findById(id);
+    }
+
     @PostMapping("/product/store")
     fun storeProduct(@RequestBody product: ProductResponseDTO): Product{
         return productService.store(product);
     }
-
 
 
 }
--- src/main/kotlin/com/cleanFood/services/ProductService.kt
+++ src/main/kotlin/com/cleanFood/services/ProductService.kt
@@ -2,9 +2,11 @@ package com.cleanFood.services
 
 import com.cleanFood.dtos.ProductResponseDTO
 import com.cleanFood.entitys.Product
+import com.cleanFood.exceptions.ExceptionProductNotFounded
 import com.cleanFood.repository.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.Optional
 import kotlin.Exception
 
 @Service
@@ -15,15 +17,37 @@ class ProductService(){
     fun store(data:ProductResponseDTO):Product{
         try{
             var product: Product = Product(name = data.name, price = data.price, stock = data.stock, order = null);
-
-
             productRepository.save(product);
-
             return product;
-
         }
         catch (ex: Exception){
             throw Exception("error:" + ex.message);
+        }
+    }
+
+    fun findById(id: String):Optional<Product>{
+        try{
+            val product:Optional<Product> = this.productRepository.findById(id);
+            if(product.isEmpty){
+                throw ExceptionProductNotFounded("Produto não encontrado.")
+            }
+            return product;
+
+        }
+        catch (ex:Exception){
+            throw ExceptionProductNotFounded("Produto não encontrado.")
+        }
+    }
+
+    fun findMany():List<Product>{
+        try{
+            val productList: List<Product> = this.productRepository.findAll();
+
+            return productList;
+
+        }
+        catch (ex:Exception){
+            throw Exception("error" + ex.message);
         }
     }
 }
